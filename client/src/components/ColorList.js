47d0ec092587116c1963ab4bdfe9f558630f9c4c
emilme <emilme@sir.mle>
2020-07-24 09:09:42 -0700

    Delete & Put working ERROR=> page updates on refresh not on submit

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import {axiosWithAuth} from '../utils/axiosWithAuth'
 
 const initialColor = {
   color: "",
@@ -21,12 +22,26 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+    .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+    .then(res => {
+      console.log('EEEEEEEEEEE',res)
+      setColorToEdit(res.data)
+      setEditing(false);
+    })
+    .catch(err => console.log(err));
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`http://localhost:5000/api/colors/${color.id}`)
+      .then((res) => {
+        console.log('DDDDDDDDDD', res)
+      })
+      .catch(err => console.log(err));
   };
-
+      
   return (
     <div className="colors-wrap">
       <p>colors</p>
